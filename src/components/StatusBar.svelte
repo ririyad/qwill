562ef63802c0ff1fc404e3ad74421bd0b6050232
ririyad <rasheduliriyad@gmail.com>
2026-05-15 01:07:37 +0600

    style: polish editor surface states

--- src/components/StatusBar.svelte
+++ src/components/StatusBar.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { readingTime, wordCount } from '../stores/editor.store';
+  import { isDirty, isSaving, readingTime, wordCount } from '../stores/editor.store';
 </script>
 
 <footer class="status-bar">
   <span>{$wordCount} words</span>
   <span>{$readingTime} min read</span>
   <span class="status-spacer"></span>
-  <span>Saved locally</span>
+  <span>{#if $isSaving}Saving...{:else if $isDirty}Unsaved{:else}Saved locally{/if}</span>
 </footer>
